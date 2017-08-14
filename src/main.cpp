5948646a696145603eba08934c7484cdd6305d55
Tica Bogdan <tica.bogdan@yahoo.ro>
2017-08-14 19:07:41 +0300

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -7,89 +7,12 @@
 
 Handler *handler = new Handler();
 
-WiFiClient *client = new WiFiClient();
-
-WebSocketClient *webSocketClient = new WebSocketClient();
-
-char path[] = "/ws";
-char host[] = "unice1.tk";
-
-
 void setup() {
 
-        handler->setup();
-
-    return;
-
-
-    Serial.begin(115200);
-
-    WiFi.begin("T_I_K", "P@r0l@123");
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    delay(2000);
-
-    if (client->connect("unice1.tk", 80)) {
-        Serial.println("Connected");
-    } else {
-        Serial.println("Connection failed.");
-        while (1) {
-            // Hang on failure
-        }
-    }
-
-    webSocketClient->path = path;
-    webSocketClient->host = host;
-    if (webSocketClient->handshake(*client)) {
-        Serial.println("Handshake successful");
-    } else {
-        Serial.println("Handshake failed.");
-        while (1) {
-            // Hang on failure
-        }
-    }
-
-
+    handler->setup();
 }
 
 void loop() {
 
-//    String data;
-
-//    if (client->connected()) {
-//
-//        webSocketClient->getData(data);
-//        if (data.length() > 0) {
-//            Serial.print("Received data: ");
-//            Serial.println(data);
-//        }
-//
-//        // capture the value of analog 1, send it along
-////        pinMode(1, INPUT);
-//        data = String(analogRead(1));
-//
-////        webSocketClient.sendData(data);
-//
-//    } else {
-//        Serial.println("Client disconnected.");
-//        while (1) {
-//            // Hang on disconnect.
-//        }
-//    }
-
-    // wait to fully let the client disconnect
-//    delay(3000);
-
-
-//    handler->loop();
-
+    handler->loop();
 }
